3a575d357a4bcb969e57c43b198aa40716020086
Kelly Heilman <kellyannheilman@gmail.com>
2019-09-27 13:46:50 -0700

    Update gssurgo_lookup_scripts.py
    Added some more comments to make it a bit easier to follow

--- gssurgo_lookup_scripts.py
+++ gssurgo_lookup_scripts.py
@@ -9,14 +9,23 @@
 #Copy and paste this script into the python terminal in ARCGIS *AFTER* Adding the MapUnitRaster_10m object 
 #And aggregation file from GGSURGO_aggregation.R
 
+################################ OUTLINE ################################
+# 1. Join the state leve edited component tables from GSSURGO_aggregation.R to the 10 m resolution state level GSSURGO rasters
+# 2. Perform Lookups for each variable to produce individual state rasters for each variable of interest
+# 3. Mosaic all states together (most time intensive process) to create domain-wide 10 meter rasters for each soil variable
+# 4. Aggregate domain-wide soil variables to the scale of interest (i.e 8km)
+##########################################################################
 
 ###WARNING#### Sometimes ARCGIS reads in the component table as a STRING instead of number, the fix is to manually change 
 ##the columns names in excel for the offending component table to append "_s", and then use the var loop in the second code lbock
 
+--------------------------------------------------------------------------
+# 1. Join the state leve edited component tables & GSSURGO rasters
+--------------------------------------------------------------------------
 
-#Script to aggregate USDA geospatial data
-filedir = "C:/Users/paleolab/Documents/soils/data/" #Where final data is to be exported.
-filedir = "E:/Kelly/soils/"
+# Script to aggregate USDA geospatial data
+#filedir = "C:/Users/paleolab/Documents/soils/data/" #Where final data is to be exported.
+filedir = "E:/Kelly/soils/" 
 #Set up initial arguments
 N = "2" #When ARCGIS copies a data table, it exports it as "Export_Output_N as default
 state = "NY" #State for current map
@@ -35,13 +44,23 @@ arcpy.CalculateField_management(in_table=tablename, field="mukey_s", expression=
 #Join to 10m raster using mukey_s field
 arcpy.AddJoin_management(in_layer_or_view=raster10m, in_field="MUKEY", join_table=tablename, join_field="mukey_s", join_type="KEEP_ALL")
 
+
+--------------------------------------------------------------------------
+# 2. Perform Lookups for each variable 
+--------------------------------------------------------------------------
+
 filedir = "E:/Kelly/soils/"
 params = ["cec7", "silt", "clay", "caco3", "water_stor", "ksat", "sand"]
 #Loop uver aggregation and write files to file dir.
 for val in params:
   arcpy.gp.Lookup_sa( raster10m , tablename1+val, filedir + "lookup" + val[:3] + state )
-  arcpy.gp.Aggregate_sa("lookup" + val[:3]  + state, filedir + state + "1km_" + val[:3], "100", "MEAN", "EXPAND", "DATA")
-  arcpy.gp.Aggregate_sa("lookup" + val[:3]  + state, filedir + state + "8km_" + val[:3], "800", "MEAN", "EXPAND", "DATA")
+  #arcpy.gp.Aggregate_sa("lookup" + val[:3]  + state, filedir + state + "1km_" + val[:3], "100", "MEAN", "EXPAND", "DATA")
+  #arcpy.gp.Aggregate_sa("lookup" + val[:3]  + state, filedir + state + "8km_" + val[:3], "800", "MEAN", "EXPAND", "DATA")
+
+
+--------------------------------------------------------------------------
+# 3. Mosaic all states together
+--------------------------------------------------------------------------
 
 
 ####
@@ -49,22 +68,22 @@ for val in params:
 dist = ["100", "800"]
 params = ["sand", "silt", "clay", "cec7", "caco3", "water_stor", "ksat"]
 filepath = "E:/Kelly/soils"
-for val in dist:
-	for var in params:
-		x = filepath+"/Indiana/lookup"+var[:3]+"in;"+filepath+"/Wisconsin/lookup"+var[:3]+"wi;"+filepath+"/Illinois/lookup"+var[:3]+"il;"+filepath+"/Michigan/lookup"+var[:3]+"mi;"+filepath+"/Minnesota/lookup"+var[:3]+"mn"
-		arcpy.MosaicToNewRaster_management(input_rasters=x, output_location="E:/Kelly/soils", raster_dataset_name_with_extension="mosaic"+ var + ".tif", coordinate_system_for_the_raster="", pixel_type="32_BIT_FLOAT", cellsize="", number_of_bands="1", mosaic_method="BLEND", mosaic_colormap_mode="FIRST")
-		arcpy.gp.Aggregate_sa("mosaic"+var+".tif", "E:/Kelly/soils/fs_"+val[:1] +"_km_"+var[:3], val, "MEAN", "EXPAND", "DATA")
+#for val in dist:
+for var in params:
+	x = filepath+"/Indiana/lookup"+var[:3]+"in;"+filepath+"/Wisconsin/lookup"+var[:3]+"wi;"+filepath+"/Illinois/lookup"+var[:3]+"il;"+filepath+"/Michigan/lookup"+var[:3]+"mi;"+filepath+"/Minnesota/lookup"+var[:3]+"mn"
+	arcpy.MosaicToNewRaster_management(input_rasters=x, output_location="E:/Kelly/soils", raster_dataset_name_with_extension="mosaic"+ var + ".tif", coordinate_system_for_the_raster="", pixel_type="32_BIT_FLOAT", cellsize="", number_of_bands="1", mosaic_method="BLEND", mosaic_colormap_mode="FIRST")
+	#arcpy.gp.Aggregate_sa("mosaic"+var+".tif", "E:/Kelly/soils/fs_"+val[:1] +"_km_"+var[:3], val, "MEAN", "EXPAND", "DATA")
 
 
 #Mosaic all the states.
 dist = ["100", "800"]
 params = ["sand", "silt", "clay", "cec7", "caco3", "water_stor", "ksat"]
 filepath = "E:/Kelly/soils"
-for val in dist:
-	for var in params:
-		#x = filepath+"/Minnesota/lookup"+var[:3]+"mn;" +filepath+"/Indiana/lookup"+var[:3]+"in;"+filepath+"/Wisconsin/lookup"+var[:3]+"wi;"+filepath+"/Illinois/lookup"+var[:3]+"il;"+filepath+"/Michigan/lookup"+var[:3]+"mi;" +filepath+"/Pennslyvania/lookup"+var[:3]+"pa;"+filepath+"/New York/lookup"+var[:3]+"ny;"+filepath+"/New Jersey/lookup"+var[:3]+"nj;"+filepath+"/Massachusetts/lookup"+var[:3]+"ma;"+filepath+"/Vermont/lookup"+var[:3]+"vt;"+filepath+"/New Hampshire/lookup"+var[:3]+"nh;"+filepath+"/Maine/lookup"+var[:3]+"me;"+filepath+"/Connecticut/lookup"+var[:3]+"ct;"+filepath+"/Rhode Island/lookup"+var[:3]+"ri;"+filepath+"/Delaware/lookup"+var[:3]+"de;"+filepath+"/Ohio/lookup"+var[:3]+"oh;"+filepath+"/Pennsylvania/lookup"+var[:3]+"pa"
-		arcpy.MosaicToNewRaster_management(input_rasters=x, output_location="E:/Kelly/soils", raster_dataset_name_with_extension="kevmos"+ var + ".tif", coordinate_system_for_the_raster="", pixel_type="32_BIT_FLOAT", cellsize="", number_of_bands="1", mosaic_method="BLEND", mosaic_colormap_mode="FIRST")
-		arcpy.gp.Aggregate_sa("kevmos"+var+".tif", "E:/Kelly/soils/kev_"+val[:1] +"_km_"+var[:3], val, "MEAN", "EXPAND", "DATA")
+#for val in dist:
+for var in params:
+	#x = filepath+"/Minnesota/lookup"+var[:3]+"mn;" +filepath+"/Indiana/lookup"+var[:3]+"in;"+filepath+"/Wisconsin/lookup"+var[:3]+"wi;"+filepath+"/Illinois/lookup"+var[:3]+"il;"+filepath+"/Michigan/lookup"+var[:3]+"mi;" +filepath+"/Pennslyvania/lookup"+var[:3]+"pa;"+filepath+"/New York/lookup"+var[:3]+"ny;"+filepath+"/New Jersey/lookup"+var[:3]+"nj;"+filepath+"/Massachusetts/lookup"+var[:3]+"ma;"+filepath+"/Vermont/lookup"+var[:3]+"vt;"+filepath+"/New Hampshire/lookup"+var[:3]+"nh;"+filepath+"/Maine/lookup"+var[:3]+"me;"+filepath+"/Connecticut/lookup"+var[:3]+"ct;"+filepath+"/Rhode Island/lookup"+var[:3]+"ri;"+filepath+"/Delaware/lookup"+var[:3]+"de;"+filepath+"/Ohio/lookup"+var[:3]+"oh;"+filepath+"/Pennsylvania/lookup"+var[:3]+"pa"
+	arcpy.MosaicToNewRaster_management(input_rasters=x, output_location="E:/Kelly/soils", raster_dataset_name_with_extension="kevmos"+ var + ".tif", coordinate_system_for_the_raster="", pixel_type="32_BIT_FLOAT", cellsize="", number_of_bands="1", mosaic_method="BLEND", mosaic_colormap_mode="FIRST")
+	#arcpy.gp.Aggregate_sa("kevmos"+var+".tif", "E:/Kelly/soils/kev_"+val[:1] +"_km_"+var[:3], val, "MEAN", "EXPAND", "DATA")
 
 
 
@@ -94,14 +113,18 @@ x = filepath+"/Minnesota/lookup"+var[:3]+"mn;"+
 dist = ["100", "800"]
 params = ["sand", "silt", "clay", "cec7", "caco3", "water_stor", "ksat"]
 filepath = "E:/Kelly/soils"
-for val in dist:
-	for var in params:
-		#x = filepath+"/Minnesota/lookup"+var[:3]+"mn;" +filepath+"/Indiana/lookup"+var[:3]+"in;"+filepath+"/Wisconsin/lookup"+var[:3]+"wi;"+filepath+"/Illinois/lookup"+var[:3]+"il;"+filepath+"/Michigan/lookup"+var[:3]+"mi;" +filepath+"/Pennslyvania/lookup"+var[:3]+"pa;"+filepath+"/New York/lookup"+var[:3]+"ny;"+filepath+"/New Jersey/lookup"+var[:3]+"nj;"+filepath+"/Massachusetts/lookup"+var[:3]+"ma;"+filepath+"/Vermont/lookup"+var[:3]+"vt;"+filepath+"/New Hampshire/lookup"+var[:3]+"nh;"+filepath+"/Maine/lookup"+var[:3]+"me;"+filepath+"/Connecticut/lookup"+var[:3]+"ct;"+filepath+"/Rhode Island/lookup"+var[:3]+"ri;"+filepath+"/Delaware/lookup"+var[:3]+"de;"+filepath+"/Ohio/lookup"+var[:3]+"oh;"+filepath+"/Pennsylvania/lookup"+var[:3]+"pa"
-		arcpy.MosaicToNewRaster_management(input_rasters=x, output_location="E:/Kelly/soils", raster_dataset_name_with_extension="kevmos"+ var + ".tif", coordinate_system_for_the_raster="", pixel_type="32_BIT_FLOAT", cellsize="", number_of_bands="1", mosaic_method="BLEND", mosaic_colormap_mode="FIRST")
-		arcpy.gp.Aggregate_sa("kevmos"+var+".tif", "E:/Kelly/soils/kev_"+val[:1] +"_km_"+var[:3], val, "MEAN", "EXPAND", "DATA")
+#for val in dist:
+for var in params:
+	#x = filepath+"/Minnesota/lookup"+var[:3]+"mn;" +filepath+"/Indiana/lookup"+var[:3]+"in;"+filepath+"/Wisconsin/lookup"+var[:3]+"wi;"+filepath+"/Illinois/lookup"+var[:3]+"il;"+filepath+"/Michigan/lookup"+var[:3]+"mi;" +filepath+"/Pennslyvania/lookup"+var[:3]+"pa;"+filepath+"/New York/lookup"+var[:3]+"ny;"+filepath+"/New Jersey/lookup"+var[:3]+"nj;"+filepath+"/Massachusetts/lookup"+var[:3]+"ma;"+filepath+"/Vermont/lookup"+var[:3]+"vt;"+filepath+"/New Hampshire/lookup"+var[:3]+"nh;"+filepath+"/Maine/lookup"+var[:3]+"me;"+filepath+"/Connecticut/lookup"+var[:3]+"ct;"+filepath+"/Rhode Island/lookup"+var[:3]+"ri;"+filepath+"/Delaware/lookup"+var[:3]+"de;"+filepath+"/Ohio/lookup"+var[:3]+"oh;"+filepath+"/Pennsylvania/lookup"+var[:3]+"pa"
+	arcpy.MosaicToNewRaster_management(input_rasters=x, output_location="E:/Kelly/soils", raster_dataset_name_with_extension="kevmos"+ var + ".tif", coordinate_system_for_the_raster="", pixel_type="32_BIT_FLOAT", cellsize="", number_of_bands="1", mosaic_method="BLEND", mosaic_colormap_mode="FIRST")
+	#arcpy.gp.Aggregate_sa("kevmos"+var+".tif", "E:/Kelly/soils/kev_"+val[:1] +"_km_"+var[:3], val, "MEAN", "EXPAND", "DATA")
 	
-	
-	
+
+--------------------------------------------------------------------------
+# 4. Aggregate to the scale of interest (i.e 8km)
+--------------------------------------------------------------------------
+
+#note: to do the aggregation to 8km grid below, the mosaic tif of interest needs to be loaded into ArcGIS
 	
 
 # If you want to change the aggregation for whole paleon domain:
@@ -119,5 +142,5 @@ for val in dist:
 		#arcpy.MosaicToNewRaster_management(input_rasters=x, output_location="E:/Kelly/soils", raster_dataset_name_with_extension="kevmos"+ var + ".tif", coordinate_system_for_the_raster="", pixel_type="32_BIT_FLOAT", cellsize="", number_of_bands="1", mosaic_method="BLEND", mosaic_colormap_mode="FIRST")
 		arcpy.gp.Aggregate_sa("kevmos"+var+".tif", "E:/Kelly/soils/kev_"+val[:1] +"_km_"+var[:3], val, "MEAN", "EXPAND", "DATA")
 	
-
+# You may need to then manually export the aggregated 8km rasters to a .tif file in order to make them readable in R (this is frustrating)
 		
